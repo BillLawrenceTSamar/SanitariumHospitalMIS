ede6b4d2690f5e81d918ef96b7c537876bd77ba8
Bill-Lawrence <bill.samar@nmsc.edu.ph>
2018-07-10 23:24:46 +0800

    Development
    Changes to Helpers > DatabaseController

--- SanitariumHospitalMIS/Helpers/DatabaseController.cs
+++ SanitariumHospitalMIS/Helpers/DatabaseController.cs
@@ -1,12 +1,77 @@
-﻿using System;
+﻿// Author: Bill-Lawrence T. Samar
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace SanitariumHospitalMIS.Helpers
 {
-    class DatabaseController
+    public class DatabaseController
     {
+        private String _MySQLConnectionString = "";
+        private MySqlConnection _MySQLConnection;
+
+        public DatabaseController(String hostname,String username,String password)
+        {
+            _MySQLConnectionString = "";
+        }
+
+        public bool PerformQuery()
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return false;
+        }
+
+        public bool PerformNonQuery()
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return false;
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                _MySQLConnection.ConnectionString = _MySQLConnectionString;
+                _MySQLConnection.Open();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }   
     }
 }
