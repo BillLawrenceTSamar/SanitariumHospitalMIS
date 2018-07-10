7c19ca69815529c6f7e01e33f42cca2ab03273e5
Bill-Lawrence <bill.samar@nmsc.edu.ph>
2018-07-11 00:00:40 +0800

    Development

--- SanitariumHospitalMIS/Views/StaffManagementForm.Designer.cs
+++ SanitariumHospitalMIS/Views/StaffManagementForm.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // StaffManagementForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 538);
+            this.Name = "StaffManagementForm";
             this.Text = "StaffManagementForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
